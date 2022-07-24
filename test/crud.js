b70d4973f7bad800da069ebbfa309112d24c2658
Andres Valderrama <andres.valderrama@zemoga.com>
2022-07-24 15:13:53 -0500

    Unit tests for crud functions

---
+++ test/crud.js
@@ -0,0 +1,26 @@
+const tasks = []
+
+//CRUD
+//Create
+//Read
+//Update
+//Delete
+
+//button add task was pressed
+function addTask() {}
+
+//press button delete
+function removeTask(id) {}
+
+//press button update task
+function updateTask(id) {}
+
+//read a task
+function readTask(id) {}
+
+module.exports = {
+    addTask,
+    removeTask,
+    updateTask,
+    readTask
+}
---
+++ test/crud.test.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert
+const crud = require('./crud.js')
+
+describe("Testing CRUD Methods", function () {
+  describe("Testing addTask", function() {
+    it('should start as empty array', function() {})
+    it('should define the id as an integer autoincrement', function() {})
+    it('should define completed as false if it is not defined', function() {})
+    it('should ignore if completed is a non boolean type', function() {})
+    it('should return the task added', function() {})
+    it('should require an id, title, completed', function() {})
+    it('should ignore no supported properties', function() {})
+    it('should capatilize just the first letter of the title', function() {})
+  })
+})
