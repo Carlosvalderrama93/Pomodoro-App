1743f2790c63fdd070dce1bcfa1269f308e8c660
Carlos Valderrama <_CarlosValderrama@outlook.com>
2022-08-23 20:29:03 -0500

    Fixing test

--- src/todo-test/newtask.test.mjs
+++ src/todo-test/newtask.test.mjs
@@ -1,7 +1,7 @@
 import { assert } from "chai"
 import DOM from "@testing-library/dom"
 
-import newTaskEl, {template as newTaskTmpl} from "../templates/new-task.js"
+import newTaskEl from "../templates/new-task.js"
 
 describe("New Task", () => {
   it("should render a h3 title with value equal to Add Task", function(){
@@ -13,13 +13,13 @@ describe("New Task", () => {
 
   it("should disabled create task button when app starts", function(){
     //Verificar que el botón esté desabilitado
-    const newTaskCreate = newTaskEl.querySelector(".new-task-create");
-    assert.isTrue(newTaskCreate.disabled)
+    const newTaskCreateEl = newTaskEl.querySelector(".new-task-button");
+    assert.isTrue(newTaskCreateEl.disabled)
   })
   it("should enable create task button when input have a text", function(){
     /*1. entrar al Input, 2. introducir texto, 3. habilitar el botón. 4. test*/
-    const newTaskTitle = newTaskEl.querySelector(".new-task-title");
-    const newTaskCreate = newTaskEl.querySelector(".new-task-create");
+    const newTaskTitle = newTaskEl.querySelector(".new-task-input");
+    const newTaskCreate = newTaskEl.querySelector(".new-task-button");
 
 
     DOM.fireEvent.keyUp(newTaskTitle, {
@@ -33,8 +33,8 @@ describe("New Task", () => {
 
   it("should disable create task button when input is empty", function (){
     /*1. entrar al input, 2. escribo un texto, 3. borro el texto, 4. test*/
-    const newTaskTitle = newTaskEl.querySelector(".new-task-title");
-    const newTaskCreate = newTaskEl.querySelector(".new-task-create");
+    const newTaskTitle = newTaskEl.querySelector(".new-task-input");
+    const newTaskCreate = newTaskEl.querySelector(".new-task-button");
 
     DOM.fireEvent.keyUp(newTaskTitle, {
       target:{
@@ -53,8 +53,8 @@ describe("New Task", () => {
   })
 
  it("should check if the input is empty once the button is clicked", function(){
-    const newTaskInput = newTaskEl.querySelector(".new-task-title");
-    const newTaskCreate = newTaskEl.querySelector(".new-task-create");
+    const newTaskInput = newTaskEl.querySelector(".new-task-input");
+    const newTaskCreate = newTaskEl.querySelector(".new-task-button");
     // 1) Ingresar al input
     // 2) ingresar texto
     // 3) clickear el botón
@@ -69,8 +69,8 @@ describe("New Task", () => {
 
   it ("should disable the create task button when this was clicked", function (){
     /*1. seleccionar el input, 2. ingresar texto, 3. clickear botón, 4. desabilitar el botón*/
-    const newTaskInput = newTaskEl.querySelector(".new-task-title");
-    const newTaskCreate = newTaskEl.querySelector(".new-task-create");
+    const newTaskInput = newTaskEl.querySelector(".new-task-input");
+    const newTaskCreate = newTaskEl.querySelector(".new-task-button");
 
     DOM.fireEvent.keyUp(newTaskInput, {
       target:{
