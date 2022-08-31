27a539a34912e0a5aecbf2aca74e55315c89434d
Andres Valderrama <andres.valderrama@zemoga.com>
2022-08-30 20:22:50 -0500

    Server 2
    /tareas [GET, POST]
    /home [GET]
    /about [GET]
    /works [GET]

--- server/index.js
+++ server/index.js
@@ -1,18 +1,47 @@
 import express from "express"
 
+import db from "./db.js"
+
 const PORT = process.env.PORT || 3000
 const server = express()
 
+// CONFIG
+server.use(express.json())
+server.use(express.urlencoded())
 server.set('views', './server/views')
 server.set('view engine', 'ejs')
 
-// VERBS
-// POST   = CREATE
-// GET    = READ
-// PUT    = UPDATE
-// DELETE = DELETE
 server.get("/", (req, res) => {
   res.render("home")
+})
+
+server.get("/about", (req, res) => {
+  res.render("about")
+})
+
+server.get("/works", (req, res) => {
+  res.render("works")
+})
+
+// READ
+server.get("/tareas", (req, res) => {
+  res.render("tareas", {tasks: db.data.tasks})
+})
+
+// server.get("/tasks/:id", (req, res) => {})
+
+// CREATE
+server.post("/tareas", (req, res) => {
+  const { title } = req.body
+  const tasks = db.data.tasks
+  const newTask = {
+    title: title,
+    id: tasks.length,
+    completed: fals,
+  }
+
+  tasks.push(newTask)
+  res.redirect("/tareas")
 })
 
 server.listen(PORT, () => {
